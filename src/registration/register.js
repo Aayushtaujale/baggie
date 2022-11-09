82021b53e777ad805a555285df34bdca43ff0e25
ShabdaKafle <shabdakafle@gmail.com>
2022-11-09 21:22:31 +0545

    signup

--- src/registration/register.js
+++ src/registration/register.js
@@ -1,119 +1,114 @@
+import '../css/register.css';
+
+
 const Register=()=>{
-return(
-<div>
-<section class="h-100 bg-dark">
-  <div class="container py-5 h-100">
-    <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col">
-        <div class="card card-registration my-4">
-          <div class="row g-0">
-            {/* <div class="col-xl-6 d-none d-xl-block">
-              <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/img4.webp"
-                alt="Sample photo" class="img-fluid"
-                style={"{border-top-left-radius: .25rem; border-bottom-left-radius: .25rem;}"}/>    
-            </div> */}
-            <div class="col-xl-6">
-              <div class="card-body p-md-5 text-black">
-                <h3 class="mb-5 text-uppercase">Register Here</h3>
+    
+    
+    return(
+ 
+        <div className="containerfirst">
+             
+             <div className='containersecond'>
+            <div className="row">
+                <div className="col-md-9">
+               
 
-                <div class="row">
-                  <div class="col-md-6 mb-4">
-                    <div class="form-outline">
-                      <input type="text" id="form3Example1m" class="form-control form-control-lg" />
-                      <label class="form-label" for="form3Example1m">First name</label>
-                    </div>
-                  </div>
-                  <div class="col-md-6 mb-4">
-                    <div class="form-outline">
-                      <input type="text" id="form3Example1n" class="form-control form-control-lg" />
-                      <label class="form-label" for="form3Example1n">Last name</label>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="form-outline mb-4">
-                  <input type="text" id="form3Example8" class="form-control form-control-lg" />
-                  <label class="form-label" for="form3Example8">Address</label>
-                </div>
-
-                {/* <div class="d-md-flex justify-content-start align-items-center mb-4 py-2">
-
-                  <h6 class="mb-0 me-4">Gender: </h6>
-
-                  <div class="form-check form-check-inline mb-0 me-4">
-                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="femaleGender"
-                      value="option1" />
-                    <label class="form-check-label" for="femaleGender">Female</label>
-                  </div>
-
-                  <div class="form-check form-check-inline mb-0 me-4">
-                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="maleGender"
-                      value="option2" />
-                    <label class="form-check-label" for="maleGender">Male</label>
-                  </div>
-
-                  <div class="form-check form-check-inline mb-0">
-                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="otherGender"
-                      value="option3" />
-                    <label class="form-check-label" for="otherGender">Other</label>
-                  </div>
-
-                </div> */}
-
-                {/* <div class="row">
-                  <div class="col-md-6 mb-4">
-
-                    <select class="select">
-                      <option value="1">State</option>
-                      <option value="2">Option 1</option>
-                      <option value="3">Option 2</option>
-                      <option value="4">Option 3</option>
-                    </select>
-
-                  </div>
-                  <div class="col-md-6 mb-4">
-
-                    <select class="select">
-                      <option value="1">City</option>
-                      <option value="2">Option 1</option>
-                      <option value="3">Option 2</option>
-                      <option value="4">Option 3</option>
-                    </select>
-
-                  </div>
-                </div> */}
-
-
-                <div class="form-outline mb-4">
-                  <input type="text" id="form3Example97" class="form-control form-control-lg" />
-                  <label class="form-label" for="form3Example97">Email ID</label>
-                </div>
-
-                <div class="form-outline mb-4">
-                  <input type="text" id="form3Example99" class="form-control form-control-lg" />
-                  <label class="form-label" for="form3Example99">Password</label>
-                </div>
-
-                <div class="form-outline mb-4">
-                  <input type="text" id="form3Example99" class="form-control form-control-lg" />
-                  <label class="form-label" for="form3Example99">Cofirm Password</label>
-                </div>
-
-    <div class="d-flex justify-content-end pt-3">
-                  {/* <button type="button" class="btn btn-light btn-lg">Reset all</button> */}
-                  <button type="button" class="btn btn-warning btn-lg ms-2">Submit form</button>
-    </div>
-
-              </div>
+                <h2 className='h2'>Register Here</h2>
+          
+            <form>
+            <div class="form-group">
+               
+                <label className='register-label' >Firstname</label>
+               
+                
+                <input ttype="text"
+                  className="register-input"  placeholder="Firstname"/>
+               
             </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-        </div>
+            <br/>
 
-)
+            <div class="form-group">
+            
+                <label className='register-label' >Lastname</label>
+                
+
+                <input type="text"
+                  className="register-input"  placeholder="Lastname" />
+                
+            
+            </div>
+            <br/>
+
+            <div class="form-group">
+            
+                <label className='register-label'>Contact</label>
+                
+
+                <input type="text"
+                  className="register-input" placeholder="Contact" />
+                
+            </div>
+            <br/>
+
+            <div class="form-group">
+            
+                <label className='register-label'>Address</label>
+                
+
+                <input type="text"
+                  className="register-input" placeholder="Address" />
+                
+            </div>
+            <br/>
+
+            <div class="form-group">
+            
+                <label className='register-label'>Email</label>
+                
+                <input type="text"
+                  className="register-input"
+
+                  placeholder="Email"/>
+                
+            </div>
+            <br/>
+            <div class="form-group">
+            
+                <label className='register-label'>Password</label>
+                
+
+                <input type="text"
+                  className="register-input"   placeholder="Password" />
+                
+            </div>
+            <br/>
+
+            <div class="form-group">
+            
+                <label className='register-label'>Confirm Password</label>
+                
+
+                <input type="text"
+                  className="register-input"  placeholder="Confirm Password" />
+                
+            </div>
+            <br/>
+    
+            
+            <button type="submit" className="button">Register</button>
+           
+            </form>
+
+
+
+                </div>
+            </div>
+            </div>
+            
+        </div>
+    )
+
+
 }
+
 export default Register;
