f2b50b2c2b239f5a37469907d27368388f3997de
aayush <aayushtaujale009@gmail.com>
2022-11-11 01:32:04 +0545

    files changed

--- src/components/login.js
+++ src/components/login.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 import "../styles/login.css";
 
+
+// -----------------------------------------------------
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -50,3 +52,5 @@ const Login = () => {
 };
 
 export default Login;
+
+//////////////////////////////
