386aad865da4dba6a877802f2a4cbe104e79d090
Bipas666 <koiralabipas@gmail.com>
2022-11-25 21:41:08 +0545

    improvise

--- src/components/add_category.js
+++ src/components/add_category.js
@@ -1,174 +1,188 @@
-// import { fab } from '@fortawesome/free-brands-svg-icons'
 import "../styles/add_category.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faImage, faUpload } from "@fortawesome/free-solid-svg-icons";
 const CategoryShowAdd = () => {
-   
-    const [categoryName, setCategoryName] = useState('');
-    const[message, setMessage]=useState('');
-    const [categoryDetails, setCategoryDetails]= useState([]);
-    const[categoryImage, setCategoryImage]= useState('');
+  const [categoryName, setCategoryName] = useState("");
+  const [message, setMessage] = useState("");
+  const [categoryDetails, setCategoryDetails] = useState([]);
+  const [categoryImage, setCategoryImage] = useState("");
 
+  const [categoryList, setCategoryList] = useState([]);
 
-    const [categoryList, setCategoryList]= useState([]);
+  const addCategory = (e) => {
+    e.preventDefault();
 
+    console.warn(categoryName, categoryDetails, categoryImage);
+    // console.warn(title, content)
+    const formData = new FormData();
 
+    formData.append("categoryName", categoryName);
+    formData.append("categoryDetails", categoryDetails);
+    formData.append("categoryImage", categoryImage);
 
-    const addCategory=(e)=>{
-        e.preventDefault();
-    
-            console.warn(categoryName, categoryDetails, categoryImage)
-            // console.warn(title, content)
-            const formData= new FormData();
-          
-            formData.append("categoryName" ,categoryName);
-            formData.append("categoryDetails" , categoryDetails);
-            formData.append("categoryImage" , categoryImage);
-    
-            const config = {
-                headers: {
-                  Authorization: "Bearer " + localStorage.getItem("ticket"),
-                },
-              };
-            axios
-            .post("http://localhost:90/admin/addcategory",formData  )
-            .then(response=>{
-                //see in console. data is stored in data:{token:.............}
-                setMessage(response.data.msg);       
-    
-                    alert ("Category succesfully posted!");
-    
-                    window.location.reload();
-                
-            })
-            .catch(e=>{
-                console.log(e)
-                alert (" All fields required" + " Try again!")
-            })
-        };
+    // const config = {
+    //     headers: {
+    //       Authorization: "Bearer " + localStorage.getItem("ticket"),
+    //     },
+    //   };
+    axios
+      .post("http://localhost:90/admin/addcategory", formData)
+      .then((response) => {
+        //see in console. data is stored in data:{token:.............}
+        setMessage(response.data.msg);
 
+        alert("Category succesfully posted!");
 
-  useEffect(()=>{
-      axios
-        .get("http://localhost:90/category/display")
-        .then((result) => {
-          console.log("category")
-          console.log(result);
-  
-          setCategoryList(result.data.data);
-        
-          
-        })
-        .catch((e) => {
-          console.log(e);
-        });
-    }, []);
-    
-  
+        window.location.reload();
+      })
+      .catch((e) => {
+        console.log(e);
+        alert(" All fields required" + " Try again!");
+      });
+  };
 
-    
-    //delete category
-    const deleteCategory = (pid) => {
-      const data = {
-        id: pid,
-      };
-      axios
-        .delete("http://localhost:90/category/delete/" + pid)
-        .then((result) => {
-          console.log(result);
-          if (result.data.status) {
-            alert("Post deleted!")
-            window.location.reload();
-          }
-        });
+  useEffect(() => {
+    axios
+      .get("http://localhost:90/category/display")
+      .then((result) => {
+        console.log("category");
+        console.log(result);
+
+        setCategoryList(result.data.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
+
+  //delete category
+  const deleteCategory = (pid) => {
+    const data = {
+      id: pid,
     };
+    axios
+      .delete("http://localhost:90/category/delete/" + pid)
+      .then((result) => {
+        console.log(result);
+        if (result.data.status) {
+          alert("Post deleted!");
+          window.location.reload();
+        }
+      });
+  };
 
-    return(
-      <body>
-       <div className="ad-cat">
-
-
-           <div className="sav-change">
-               <div className="text-center"><h2 style={{color: "#9F2893",  textAlign: 'center' }} > Add Category</h2></div>
-               {categoryImage && (
-                <div className="imagebox"> 
-
-                  <div className="imagebox2">
-                <div className="imagebox3">
-                    <img className="imagebox4"  src={URL.createObjectURL(categoryImage)} alt="" />
-                </div>
-                </div>
-                </div>
-
-      )}
-                  <form> 
-                          
-                          <div className="a-img"><label htmlFor="fileInput">
-                          <p className="pluss">+</p>
-                          <p>Image</p>
-                            </label>
-                            <input
-                              type="file"
-                              id="fileInput"
-                              style={{ display: "none" }}
-                              onChange={(e) => setCategoryImage(e.target.files[0])}
-                            /></div>
-                        <div className="categoryN"><input
-                              type="text"
-                              placeholder="Title"
-                              className="writeInput"
-                              autoFocus={true}
-                              onChange={e=>setCategoryName(e.target.value)}
-                            />
-                          </div> 
-
-
-                        <div className="a-bott">
-                          <textarea
-                                placeholder="Tell something about the characterstics..."
-                                type="text"
-                                className="writeInput writeText"
-                                onChange={e=>setCategoryDetails(e.target.value)}
-                            ></textarea>
-                        </div>
-                      
-                        <div className="pt-5">
-                          <div className="pt-5"  style={{display: "flex" ,
-                            justifyContent: "center"}}><button onClick={addCategory}  className="writeSubmit" > Post</button></div> 
-                        </div>
-                  </form>
-
+  return (
+    <body>
+      <div className="ad-cat">
+        <div className="sav-change">
+          <div className="text-center">
+            <h2 style={{ color: "#9F2893", textAlign: "center" }}>
+              {" "}
+              Add Category
+            </h2>
           </div>
+          {categoryImage && (
+            <div className="imagebox">
+              <div className="imagebox2">
+                <div className="imagebox3">
+                  <img
+                    className="upimg"
+                    src={URL.createObjectURL(categoryImage)}
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          )}
+          <form>
+            <div className="a-img">
+              <label htmlFor="fileInput">
+                <p className="imagee">
+                  Upload Image{" "}
+                  <FontAwesomeIcon className="icon" icon={faUpload} />
+                  {/* <p className="pluss">+</p> */}
+                </p>
+              </label>
+              <div className="">
+                <input
+                  type="file"
+                  id="fileInput"
+                  style={{ display: "none" }}
+                  onChange={(e) => setCategoryImage(e.target.files[0])}
+                />
+              </div>
+            </div>
+            <div className="categoryN">
+              <input
+                type="text"
+                placeholder="Title"
+                className="writeInputt"
+                autoFocus={true}
+                onChange={(e) => setCategoryName(e.target.value)}
+              />
+            </div>
 
+            <div className="a-bott">
+              <textarea
+                placeholder="Description"
+                type="text"
+                className="writeInput writeText"
+                onChange={(e) => setCategoryDetails(e.target.value)}
+              ></textarea>
+            </div>
 
+            <div className="pt-5">
+              <div
+                className="pt-5"
+                style={{ display: "flex", justifyContent: "center" }}
+              >
+                <button onClick={addCategory} className="writeSubmit">
+                  {" "}
+                  Post
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
 
-{/* This is the right side of the page where categories are shown------------------------------------------------------------------------------------------------------------------------------ */}
-       <div className="show-cat">
-           <div style={{color: "#9F2893",  textAlign: 'center' }}>
-               <h3> Categories</h3>
-               {categoryList.map((categoryy) => {
-         return (
-                       
-                        <div className="">
-                         
-                          <h6 id="chose-cat" className="cate-cat pt-2">  <h6 id="c-n">{categoryy.categoryName}</h6> <h6 id="c-u"><Link to={"/updatecate/" + categoryy._id}>Update</Link>    
-                          </h6>
-                              
-                          <h6 id="c-d"><a href="" className=" text-danger" onClick={() => {deleteCategory(categoryy._id);}}>delete</a></h6>
-                          </h6>
-                          </div>
-                           
-                           
-                     
-                       );
-                   })}
-           </div>
-          
-       </div>
-       </div>
-      </body>
-   )
-}
+        {/* This is the right side of the page where categories are shown------------------------------------------------------------------------------------------------------------------------------ */}
+        <div className="show-cat">
+          <div style={{ color: "#9F2893", textAlign: "center" }}>
+            <h3> Categories</h3>
+            {categoryList.map((categoryy) => {
+              return (
+                <div className="">
+                  <h6 id="chose-cat" className="cate-cat pt-2">
+                    {" "}
+                    <h6 id="c-n">{categoryy.categoryName}</h6>{" "}
+                    <h6 id="c-u">
+                      <Link to={"/updatecate/" + categoryy._id}>Update</Link>
+                    </h6>
+                    <h6 id="c-d">
+                      <a
+                        href=""
+                        className=" text-danger"
+                        onClick={() => {
+                          deleteCategory(categoryy._id);
+                        }}
+                      >
+                        delete
+                      </a>
+                    </h6>
+                  </h6>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </body>
+  );
+};
 
 export default CategoryShowAdd;
--- src/components/cat_update.js
+++ src/components/cat_update.js
@@ -4,191 +4,186 @@ import { useParams } from "react-router-dom";
 
 import { Link } from "react-router-dom";
 
-const UpdateCategory=()=>{
-    const  { pid }=useParams();
+const UpdateCategory = () => {
+  const { pid } = useParams();
 
-    const [categoryName, setCategoryName] = useState([]);
-    const [categoryDetails, setCategoryDetails]= useState([]);
-    const[categoryImage, setCategoryImage]= useState("")
-    const[categoryId, setId]= useState("");
+  const [categoryName, setCategoryName] = useState([]);
+  const [categoryDetails, setCategoryDetails] = useState([]);
+  const [categoryImage, setCategoryImage] = useState("");
+  const [categoryId, setId] = useState("");
 
-    
-    const [categoryD, setCategoryD]= useState([]);
-    const[cate, setCate]=useState([])
-    
+  const [categoryD, setCategoryD] = useState([]);
+  const [cate, setCate] = useState([]);
 
+  const changeImage = (e) => {
+    console.log(categoryImage);
+    e.preventDefault();
+    const form = new FormData();
+    form.append("id", categoryId);
+    form.append("pic", categoryImage);
 
-    const changeImage=(e)=>{
-      console.log(categoryImage)
-      e.preventDefault();
-      const form = new FormData();
-      form.append("id",categoryId)
-      form.append("pic", categoryImage)
+    axios.post("http://localhost:90/category/picture/update", form);
+    window.location.reload();
+  };
+  useEffect(() => {
+    axios
+      // .get("http://localhost:2099/blogpost/display")
+      .get("http://localhost:90/categorys/" + pid)
 
-      
-   
-      axios
-      .post("http://localhost:90/category/picture/update", form)
-      window.location.reload();
+      .then((result) => {
+        console.log(result);
 
-    }
-    useEffect(()=>{
-        axios
-        // .get("http://localhost:2099/blogpost/display")
-        .get("http://localhost:90/categorys/"+ pid)
-        
-        .then((result)=>{
-            console.log(result);
-            
-            setCategoryD(result.data.data);
-            setCategoryName(result.data.data.categoryName)
-            setId(result.data.data._id);
-          setCategoryImage(result.data.data.categoryImage)
-          
-            setCategoryDetails(result.data.data.categoryDetails)
-            console.log(setCategoryDetails)
-            setId(result.data.data._id);
-          setCategoryImage(result.data.data.categoryImage)
+        setCategoryD(result.data.data);
+        setCategoryName(result.data.data.categoryName);
+        setId(result.data.data._id);
+        setCategoryImage(result.data.data.categoryImage);
 
-            console.log("sdnfjjdf")
-            console.log(result.data.data.categoryName)
-           })
-        .catch()
-    }, [])
+        setCategoryDetails(result.data.data.categoryDetails);
+        console.log(setCategoryDetails);
+        setId(result.data.data._id);
+        setCategoryImage(result.data.data.categoryImage);
 
+        console.log("sdnfjjdf");
+        console.log(result.data.data.categoryName);
+      })
+      .catch();
+  }, []);
 
-    const updatec = (e) => {
-        e.preventDefault();
-        const data = {
-          categoryName:categoryName,
-          categoryDetails: categoryDetails,
-          id: pid,
-          // blogImage:blogImage
-         
-        };
-        console.log(data);
-        axios
-          .put("http://localhost:90/cat/update", data)
-          .then((result) => {
-            console.log(result);
-            alert("Category updated!")
+  const updatec = (e) => {
+    e.preventDefault();
+    const data = {
+      categoryName: categoryName,
+      categoryDetails: categoryDetails,
+      id: pid,
+      // blogImage:blogImage
+    };
+    console.log(data);
+    if (categoryName != "") {
+      axios.put("http://localhost:90/cat/update", data).then((result) => {
+        console.log(result);
+        alert("Category updated!");
         window.location.reload();
 
-  
         //   window.location.reload();
-            // setTitle(result.data.data.title);
-            // setContent(result.data.data.content);
-          })
-          .catch();
-      };
+        // setTitle(result.data.data.title);
+        // setContent(result.data.data.content);
+      });
+    } else {
+      alert("Field cannot be empty!");
+    }
+  };
 
-      useEffect(()=>{
-        axios
-          .get("http://localhost:90/category/display")
-          .then((result) => {
-            console.log("category")
-            console.log(result);
-    
-            setCate(result.data.data);
-            console.log(cate)
-             setCategoryName(cate.categoryName)
-            console.log("wath")
-            setCategoryDetails(cate.setCategoryDetails)
+  useEffect(() => {
+    axios
+      .get("http://localhost:90/category/display")
+      .then((result) => {
+        console.log("category");
+        console.log(result);
 
-            
-        
-            
-          })
-          .catch((e) => {
-            console.log(e);
-          });
-      }, []);
-      
+        setCate(result.data.data);
+        console.log(cate);
+        setCategoryName(cate.categoryName);
+        console.log("wath");
+        setCategoryDetails(cate.setCategoryDetails);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
 
-return(
+  return (
     <body>
-    <div className="ad-cat">
-   
-
-
+      <div className="ad-cat">
         <div className="sav-change">
-        
- 
-            <div className="text-center"><h3 > Update {categoryD.categoryName}</h3></div>
-            <div className="a-img"><label htmlFor="fileInput">
-            <div className="hihi-1"> 
+          <div className="text-center">
+            {" "}
+            <h2 style={{ color: "#9F2893", textAlign: "center" }}>
+              Update {categoryD.categoryName}
+            </h2>
+          </div>
+          <div className="a-img">
+            <label htmlFor="fileInput">
+              <div className="hihi-1">
+                <div className="imi-1">
+                  <div className="imi-2">
+                    <img
+                      className="imimi1"
+                      src={"http://localhost:90/" + categoryImage}
+                      alt=""
+                    />
+                  </div>
+                </div>
+              </div>
+              <p className="pluss">+</p>
+              {/* <p> Change Image</p> */}
+            </label>
+            <input
+              type="file"
+              id="fileInput"
+              style={{ display: "none" }}
+              onChange={(e) => setCategoryImage(e.target.files[0])}
+            />
+          </div>
+          <button style={{ backgroundcolor: "#9F2893" }} onClick={changeImage}>
+            Change Image
+          </button>
 
-<div className="imi-1">
-  <div className="imi-2">
-  <img className="imimi1" src={'http://localhost:90/'+ categoryImage} 
-                  alt="post" />
-    
+          <form>
+            <input
+              name="categoryName"
+              type="text"
+              class="feedback-input "
+              placeholder="categoryName"
+              value={categoryName}
+              required
+              onChange={(e) => setCategoryName(e.target.value)}
+            />
 
+            <input
+              name="categoryDetails"
+              type="text"
+              class="feedback-input "
+              placeholder="categoryName"
+              value={categoryDetails}
+              required
+              onChange={(e) => setCategoryDetails(e.target.value)}
+            />
 
-</div>
-</div>
-
-</div>
-        <p className="pluss">+</p>
-        <p> Change Image</p>
-          </label>
-          <input
-            type="file"
-            id="fileInput"
-            style={{ display: "none" }}
-            onChange={(e) => setCategoryImage(e.target.files[0])}
-          /></div>
-<button onClick={changeImage}>Done</button>
-
-            <form>      
-              <input name="categoryName" type="text" class="feedback-input " placeholder="categoryName" value={categoryName} required onChange={(e)=>setCategoryName(e.target.value)}/>
-
-              <input name="categoryDetails" type="text" class="feedback-input " placeholder="categoryName" value={categoryDetails} required onChange={(e)=>setCategoryDetails(e.target.value)}/>   
-
-
-              <div className="pt-5"><button type="submit" className="btn btn-primary pt"  onClick={updatec}> Post</button></div>
-            </form>
-    </div>
-
-
-{/* This is the right side of the page where categories are shown------------------------------------------------------------------------------------------------------------------------------ */}
-
-    <div className="show-cat">
-            <div>
-                <h3> Categories</h3>
-                {cate.map((categoryy) => {
-          return (
- 
-                         <div className="">
-                         <h6 id="chose-cat" className="cate-cat pt-2">{categoryy.categoryName} <Link to={"/updatecate/" + categoryy._id}>nbsp; Update</Link> 
-                            </h6>
-                            </div>
-                      
-                        );
-                    })}
+            <div className="pt-5">
+              <button
+                type="submit"
+                className="btn btn-primary pt"
+                onClick={updatec}
+              >
+                {" "}
+                Update
+              </button>
             </div>
-           
-
-
-
+          </form>
         </div>
 
-   
+        {/* This is the right side of the page where categories are shown------------------------------------------------------------------------------------------------------------------------------ */}
 
-    </div>
-
-
-
-   </body>
-
-
-
-
-)
-
-
-
-
-}
+        <div className="show-cat">
+          <div>
+            <h2 style={{ color: "#9F2893", textAlign: "center" }}>
+              Categories
+            </h2>
+            {cate.map((categoryy) => {
+              return (
+                <div className="">
+                  <h6 id="chose-cat" className="cate-cat pt-2">
+                    {categoryy.categoryName}{" "}
+                    <Link to={"/updatecate/" + categoryy._id}>Update</Link>
+                  </h6>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </body>
+  );
+};
 
 export default UpdateCategory;
