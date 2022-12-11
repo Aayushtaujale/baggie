e23c95b96328acb578f6ff4a199506fdfc2614aa
shaswot <shaswotmalla@gmail.com>
2022-12-11 21:48:05 +0545

    Payment mid.js routes completed

--- src/components/Mid.js
+++ src/components/Mid.js
@@ -10,30 +10,32 @@ import Addbag from "./add_bag";
 import Viewbag from "./viewadded_bags";
 import Picker from "./custombag";
 import Cart from "./add_to_cart";
+import Khalti from "./khalti";
 
 const Mid=()=>{
-    return(
-        <Routes>
-
-            {/* <Route path="/" exact component={<Home/>}/> */}
-            {/* <Route path="/" element={<Home/>}/>
+    return (
+      <Routes>
+        {/* <Route path="/" exact component={<Home/>}/> */}
+        {/* <Route path="/" element={<Home/>}/>
             <Route path="/Home" element={<Home/>}/>
             <Route path="/venues" element={<Venues/>}/>
             <Route path="/Contact" element={<ContactUs/>}/> */}
-           <Route path="/addtocart" element={<Cart/>}/> 
-           <Route path="/custombag" element={<Picker/>}/> 
-           <Route path="/viewadded_bags" element={<Viewbag/>}/>
-           <Route path="/viewbag" element={<View/>}/>
-            <Route path="/register" element={<Register/>}/>
-            <Route path="/addbag" element={<Addbag/>}/>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/addcategory" element={<CategoryShowAdd/>}/>
+        <Route path="/addtocart" element={<Cart />} />
+        <Route path="/custombag" element={<Picker />} />
+        <Route path="/viewadded_bags" element={<Viewbag />} />
+        <Route path="/viewbag" element={<View />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/addbag" element={<Addbag />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/addcategory" element={<CategoryShowAdd />} />
+        <Route path="/payment" element={<Khalti />} />
 
-            <Route path="/updatecate/:pid" element={<UpdateCategory></UpdateCategory>}></Route>
-            
-            
-        </Routes>
-    )
+        <Route
+          path="/updatecate/:pid"
+          element={<UpdateCategory></UpdateCategory>}
+        ></Route>
+      </Routes>
+    );
 }
 
 export default Mid;
